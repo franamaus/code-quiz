82aec5f1de48d6247591dbea404e0a2c9cecfe1c
Franamaus <64950097+franamaus@users.noreply.github.com>
2020-06-14 16:59:17 -0700

    first commit

---
+++ Assets/JS/script.js
@@ -0,0 +1,25 @@
+// make instruction card for CODING QUIZ CHALLENGE
+
+// add button to "start-quiz"
+
+// create 5 Q and A with 4 mulitple choices each
+
+// set timer to 75 seconds and start counting down at reveal of first question
+
+// first question with 4 multuple choice answers made with buttons
+
+// set 3 wrong, 1 correct response
+
+// correct answers shows "correct!" and move to next question
+   // wrong answer shows "wrong!", gets 10 seconds deducted then move to next question
+
+// complete 5 rounds of questions per game 
+
+// the time remaining is the score, more time left, higher score
+
+// ask user to input their name
+
+// save name with score in localStorage in order of highest to lowest
+
+//
+
---
+++ README.md
@@ -0,0 +1,25 @@
+# Code Quiz
+
+an application that 
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+### General info
+This project is 
+
+![quiz demo](./Assets/03-javascript-homework-demo.png)
+	
+### Technologies
+Project is created with:
+* HTML
+* Javascript
+	
+### Setup
+To run this project, open link in browser.
+
+
+
+
